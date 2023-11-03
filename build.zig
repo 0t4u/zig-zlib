abf708174ef6c4372d2d278a4d9b3e7d457a20f5
0t4u <61939142+0t4u@users.noreply.github.com>
2023-11-03 20:34:47 +0000

    feat: simplify build

--- build.zig
+++ build.zig
@@ -1,37 +1,96 @@
 const std = @import("std");
-const zlib = @import("zlib.zig");
 
-pub fn build(b: *std.build.Builder) void {
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
+
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    // Modules available to downstream dependencies
-    _ = b.addModule("zlib", .{
-        .source_file = .{ .path = (comptime thisDir()) ++ "/src/main.zig" },
+    // Declare module to expose to package manager to make it available to downstream
+    b.addModule("zlib", .{ .source_file = .{ .path = package_path } });
+
+    const lib = b.addStaticLibrary(.{
+        .name = "zlib",
+        // In this case the main source file is merely a path, however, in more
+        // complicated build scripts, this could be a generated file.
+        //
+        // NOTE: Commenting out this line stops "error: 'zlib.h' file not found"
+        // errors and allows the compiler to properly generate a "cimport.h.d" file
+        // to resolve zlib header imports. I have no idea why this happens.
+        //
+        // .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
     });
+    lib.linkLibC();
+    lib.addCSourceFiles(srcs, &.{"-std=c89"});
 
-    const lib = zlib.create(b, target, optimize);
-    b.installArtifact(lib.step);
+    // This declares intent for the library to be installed into the standard
+    // location when the user invokes the "install" step (the default step when
+    // running `zig build`).
+    b.installArtifact(lib);
 
-    const tests = b.addTest(.{
+    // Creates a step for unit testing. This only builds the test executable
+    // but does not run it.
+    const main_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
     });
-    lib.link(tests, .{});
+    main_tests.addIncludePath(.{ .path = include_path });
+    main_tests.linkLibrary(lib);
 
-    const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&tests.step);
+    const run_main_tests = b.addRunArtifact(main_tests);
 
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build test`
+    // This will evaluate the `test` step rather than the default, which is "install".
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&run_main_tests.step);
+
+    // Compile examples
     const bin = b.addExecutable(.{
         .name = "example1",
         .root_source_file = .{ .path = "example/example1.zig" },
         .target = target,
         .optimize = optimize,
     });
-    lib.link(bin, .{ .import_name = "zlib" });
-    b.installArtifact(bin);
+    bin.addIncludePath(.{ .path = include_path });
+    bin.linkLibrary(lib);
+    bin.addAnonymousModule("zlib", .{ .source_file = .{ .path = package_path } });
 }
 
-/// Path to the directory with the build.zig.
-fn thisDir() []const u8 {
-    return std.fs.path.dirname(@src().file) orelse unreachable;
+fn root() []const u8 {
+    return std.fs.path.dirname(@src().file) orelse ".";
 }
+
+const root_path = root() ++ "/";
+const include_path = root_path ++ "zlib";
+const package_path = root_path ++ "src/main.zig";
+
+const srcs = &.{
+    root_path ++ "zlib/adler32.c",
+    root_path ++ "zlib/compress.c",
+    root_path ++ "zlib/crc32.c",
+    root_path ++ "zlib/deflate.c",
+    root_path ++ "zlib/gzclose.c",
+    root_path ++ "zlib/gzlib.c",
+    root_path ++ "zlib/gzread.c",
+    root_path ++ "zlib/gzwrite.c",
+    root_path ++ "zlib/inflate.c",
+    root_path ++ "zlib/infback.c",
+    root_path ++ "zlib/inftrees.c",
+    root_path ++ "zlib/inffast.c",
+    root_path ++ "zlib/trees.c",
+    root_path ++ "zlib/uncompr.c",
+    root_path ++ "zlib/zutil.c",
+};
